c2d5b76a1e235f11559b77ef7c8d1c1c18f5507e
Polina Stamenova <polina.stamenova@gmail.com>
2025-02-27 15:04:16 +0200

    UI: Display flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def tailwind_class_for(flash_type)
+    case flash_type.to_sym
+    when :notice
+      'bg-green-100 border-green-400 text-green-700'
+    when :alert
+      'bg-red-100 border-red-400 text-red-700'
+    else
+      'bg-blue-100 border-blue-400 text-blue-700'
+    end
+  end
 end
